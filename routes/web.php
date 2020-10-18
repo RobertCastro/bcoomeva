c0e147e0c804017e6bf993ed1f5a2e43b10da431
Robert Castro <oneroberth@gmail.com>
2020-10-18 12:41:18 -0500

    Definir rutas projects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia\Inertia::render('Dashboard');
-})->name('dashboard');
+Route::group(["middleware" => ['auth:sanctum', 'verified']], function () {
+
+    Route::get("/dashboard", function () {
+        return Inertia::render('Dashboard');
+    })->name("dashboard");
+
+    Route::resource("projects", ProjectController::class)->except(["show"]);
+});
