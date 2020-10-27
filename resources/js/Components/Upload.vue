49981b7d8729ec33fb2f4a82b783257cd46d5991
Robert Castro <oneroberth@gmail.com>
2020-10-27 12:53:43 -0500

    Loading import

--- resources/js/Components/Upload.vue
+++ resources/js/Components/Upload.vue
@@ -1,20 +1,26 @@
 <template>
-<div>
-<input ref="csv" class="hidden" type="file" @change="onFileChange">
-<button class="bg-green-500 hover:bg-green-600 text-white font-bold py-3 mt-5 px-4 rounded" type="button" @click.prevent="selectNewPhoto">
-  <svg class="h-5 w-5 inline-block "  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-  </svg>
-    Importar Datos
-</button>
-</div>
-
+  <div>
+  <input ref="csv" class="hidden" type="file" @change="onFileChange">
+  <button class="bg-green-700 hover:bg-green-800 text-white font-bold py-3 mt-5 px-4 rounded flex items-center" type="button" @click.prevent="selectFile">
+    <div v-if="loading" class="loading-upload mr-2" />
+   
+    <svg class="h-5 w-5 inline-block mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
+</svg>
+      Importar Datos
+  </button>
+  </div>
 </template>
 
 <script>
+import LoadingButton from "./LoadingButton";
 export default {
+  components: {
+        LoadingButton
+  },
   data() {
     return {
+      loading: false,
       form: {
         csv: null,
       },
@@ -23,13 +29,15 @@ export default {
   methods: {
 
     onFileChange() {
-        const files = this.$refs.csv.files;
-        const data = new FormData();
-        data.append('csv', files[0]);
+      this.loading = true;
+      const files = this.$refs.csv.files;
+      const data = new FormData();
+      data.append('csv', files[0]);
 
         this.$inertia.post(this.route('dashboard.store'), data)
+        .then(() => this.loading = false);
     },
-    selectNewPhoto() {
+    selectFile() {
         this.$refs.csv.click();
     },
   },
