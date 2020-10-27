2c2a05cc8f8f586a932f0e82025f12d8cf8030bc
Robert Castro <oneroberth@gmail.com>
2020-10-26 19:02:09 -0500

    Botón nuevo juego

--- resources/js/Pages/Home/Game.vue
+++ resources/js/Pages/Home/Game.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <div class="h-9 bg-green-700 text-center  pt-2 pb-6">
-            <p class="text-cool-gray-50 font-bold"> Hola, {{usuario.name}}</p>
+        <div class="h-10 bg-green-700 text-center  pt-2 pb-6">
+            <p class="text-cool-gray-50 font-bold inline-block"> Hola {{usuario.name}}</p>
+            <button class="inline-block ml-15 bg-gray-100 py-0 px-3 rounded text-green-700 font-bold" v-on:click="reload">Nuevo Juego</button>
         </div>
         <div>
             <iframe style="height: 94vh" class="w-full " src="/BINGO_COOMEVA/index.html" frameborder="0"></iframe>
@@ -14,5 +15,10 @@ export default {
     props: {
         usuario: Object,
     },
+    methods: {
+        reload: function (event) {
+            location.reload();
+        }
+    }
 }
 </script>
